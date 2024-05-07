c505c92fa24d49b1121f2624dbfe9937ad01411c
sdinux36 <113924091+sdinux36@users.noreply.github.com>
2024-05-07 08:25:32 +0530

    welocome removed
    welcome page removed

--- kennel/src/App.js
+++ kennel/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import WelcomePage from "./Pages/WelcomePage";
+
 import HandlerVetOICLogin from './Components/HandlerVetOICLogin';
 
 import DirectorRegister from './Pages/DirectorRegister';
