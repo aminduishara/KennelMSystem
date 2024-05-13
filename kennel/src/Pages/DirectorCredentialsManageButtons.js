ff8b037ee772544ec15f2981303c748e0d2a292d
sdinux36 <113924091+sdinux36@users.noreply.github.com>
2024-05-13 20:07:35 +0530

    UI

--- kennel/src/Pages/DirectorCredentialsManageButtons.js
+++ kennel/src/Pages/DirectorCredentialsManageButtons.js
@@ -22,7 +22,7 @@ const DirectorCredentialManagesButtons = () => {
 
   return (
     <div style={{ backgroundColor: '#ADD8E6'}}>
-      <h1 className='text-center'>Director Credentials Manage</h1>
+      <h1 className='text-center fw-bold'>Director Credentials Manage</h1>
       <div className="container d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '90vh' }}>
         <div className="text-center mb-4"></div>
         <div className="mb-3">
